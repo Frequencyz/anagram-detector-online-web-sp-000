c133fbfa779612e206dcf4b54475028e86b05c7d
Andrew Bay <daddnbay@yahoo.com>
2019-07-17 18:19:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,12 +3,13 @@ class Anagram
   
   attr_accessor :word
 
- def initialize(word)
+  def initialize(word)
    @word = word
- end
+  end
  
- def match
-   
+  def match
+   word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+  end
  end
-  
 end
